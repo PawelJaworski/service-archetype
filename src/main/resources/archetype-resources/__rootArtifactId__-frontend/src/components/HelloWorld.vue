79b3fd0b65eba29e4fc7d42d5324a5147ec0c604
pjaworski <pawel.jaworski@altkomsoftware.pl>
2020-12-22 15:37:35 +0100

    frontend

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-frontend/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">Primary</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import 'bootstrap';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import '~bootstrap/dist/css/bootstrap.min.css';
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-frontend/src/components/OtherComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="alert alert-primary" role="alert">
+      Other component
+    </div>
+  </div>
+</template>
+
+<script>
+import 'bootstrap';
+export default {
+  name: "OtherComponent"
+}
+</script>
+
+<style scoped>
+@import '~bootstrap/dist/css/bootstrap.min.css';
+
+</style>
