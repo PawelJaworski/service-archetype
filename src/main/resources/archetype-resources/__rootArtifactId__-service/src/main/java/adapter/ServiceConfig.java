1d824380652e3e5460a62321d1ad6849897325cd
pjaworski <pawel.jaworski@altkomsoftware.pl>
2020-12-30 15:47:15 +0100

    micronaut adapters small fixes

--- src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/adapter/ServiceConfig.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-service/src/main/java/adapter/ServiceConfig.java
@@ -10,13 +10,20 @@ import ${groupId}.domain.RepositoryPoc;
 
 public final class ServiceConfig {
   @Builder.Default
-  private final RepositoryPoc repositoryPoc = new RepositoryPocInMemoryAdapter();
+  private final RepositoryPoc repositoryPoc;
+
   public final ${classPrefix}QueryFacade queryFacade;
   public final ${classPrefix}CommandFacade commandFacade;
 
   @Builder
   private ServiceConfig(RepositoryPoc repositoryPoc) {
-    this.queryFacade = new ${classPrefix}QueryFacadeImpl(repositoryPoc);
-    this.commandFacade = new ${classPrefix}CommandFacadeImpl(repositoryPoc);
+    this.repositoryPoc = newRepositoryPoc(repositoryPoc);
+
+    this.queryFacade = new ${classPrefix}QueryFacadeImpl(this.repositoryPoc);
+    this.commandFacade = new ${classPrefix}CommandFacadeImpl(this.repositoryPoc);
+  }
+
+  private RepositoryPoc newRepositoryPoc(RepositoryPoc repositoryPoc) {
+    return repositoryPoc != null ? repositoryPoc : new RepositoryPocInMemoryAdapter();
   }
 }
